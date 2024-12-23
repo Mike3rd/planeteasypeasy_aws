c0f9d049eb5eb04ad80c11295ff82663e00ab9d2
Mike3rd <91030194+Mike3rd@users.noreply.github.com>
2024-12-23 16:20:09 -0500

    test

---
+++ static/phpcomments2/config.php
@@ -0,0 +1,22 @@
+<?php
+// database hostname, you don't usually need to change this
+define('db_host','localhost');
+// database username
+define('db_user','root');
+// database password
+define('db_pass','root');
+// database name
+define('db_name','phpcomments_advanced');
+// database charset, change this only if utf8 is not supported by your language
+define('db_charset','utf8');
+// Approval required for comments?
+define('comments_approval_required',false);
+// Photo icon url input enabled
+define('photo_icon_url_enabled',false);
+// Authentication required?
+define('authentication_required',false);
+// Max nested replies
+define('max_nested_replies','2');
+// Max comment characters
+define('max_comment_chars',600);
+?>
